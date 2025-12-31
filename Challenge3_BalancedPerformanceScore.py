3d82d9ce6dca3034d566c7947236a30c4315afa3
AhzamRB <ahzambari@gmail.com>
2025-12-31 12:56:41 +0500

    redid challenge 3 cause forgot that we can't merge arrays

--- Challenge3_BalancedPerformanceScore.py
+++ Challenge3_BalancedPerformanceScore.py
@@ -1,10 +1,27 @@
 def median_merge(scoresA, scoresB):
-    scores = sorted(scoresA + scoresB)
-    n = len(scores)
-    if n % 2 == 1:
-        return scores[n//2]
-    else:
-        return (scores[n//2 - 1] + scores[n//2]) / 2
+
+    n, m = len(scoresA), len(scoresB)
+    total_length = n + m #Only combined length this time
+    a = b = 0 #Random pointers for both scores
+    previous = current = 0 #Current contains the current value, previous contains the value before it
+
+    for _ in range((total_length // 2) + 1): #Cause we can stop in the middle instead of going all the way
+
+        previous = current #Cause we need the previous value for even length arrays
+
+        #This part selects the smaller value from both and increments the pointer for the one it selected
+        # b>=m is for when scoresB runs out of scores
+        # scoresA[a] <= scoresB[b], pick from A if A[a] is smaller or equal
+        if a < n and (b >= m or scoresA[a] <= scoresB[b]):
+            current = scoresA[a]
+            a += 1
+        else:
+            current = scoresB[b]
+            b += 1
+
+    if total_length % 2 == 0:
+        return (previous + current) / 2
+    return current
 
 
 print(median_merge([1,3], [2]))
